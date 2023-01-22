fc5cf988c966a9f3fa72f48e348e4a151bff55d9
PedritoBF03 <pedrobf2003@gmail.com>
2023-01-22 23:39:50 +0100

    ejecutar 10 veces

--- prueba.sh
+++ prueba.sh
@@ -1,2 +1,7 @@
-echo "Ejecución de prueba.sh de forma remota"
-echo "Bienvenido Jenkins por facilitarnos la vida..."
+#!/bin/bash
+echo "Ejecución de prueba.sh despues de un push local"
+
+for i in {5 .. 10}
+do
+    echo "Welcome $i times"
+done
