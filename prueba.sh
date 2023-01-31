2a4a290e5ccd74a479d558df1f3ab72775f9604d
PedritoBF03 <pedrobf2003@gmail.com>
2023-01-31 22:51:01 +0100

    1º Actualizacion a VPS

--- prueba.sh
+++ prueba.sh
@@ -1,9 +1,7 @@
-echo "Bienvenido Jenkins por facilitarnos la vida"
-
 #!/bin/bash
-# echo "Ejecución de prueba.sh despues de un push local"
+echo "Ejecución de prueba.sh despues de un push local"
 
-# for i in {5 .. 100}
-# do
-#     echo "Welcome $i timesdfgdfg"
-# done
+for i in {5 .. 100}
+do
+    echo "Welcome $i timesdfgdfg"
+done
